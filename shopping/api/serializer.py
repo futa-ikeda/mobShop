9be6ad67654abd00ebd45ae3ff3879b72e20de48
Ramya Virajamangala <ramyavirajamangala@ramyas-mbp.lan>
2024-11-26 09:18:40 -0500

    added id to api view

--- shopping/api/serializer.py
+++ shopping/api/serializer.py
@@ -9,7 +9,8 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     name = CharField(max_length=200, required=True)
     price = DecimalField(max_digits=10, decimal_places=2, required=True)
     stock = IntegerField(min_value=0, max_value=100, required=True)
+
     class Meta:
         model = Product
-        fields = ('name', 'price', 'stock')
+        fields = ('id','name', 'price', 'stock')
 
